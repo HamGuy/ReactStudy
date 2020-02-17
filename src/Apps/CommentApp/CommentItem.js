bcb06de16259bb21776e1c06f8bf9b0599528534
HamGuy <wangrui15@gmail.com>
2020-02-17 11:11:40 +0800

    feat: 评论展示评论时间

--- src/Apps/CommentApp/CommentItem.js
+++ src/Apps/CommentApp/CommentItem.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
 export class CommentItem extends Component {
+
+    componentWillMount() {
+        this._updateTimeString();
+        // 每隔 5 秒执行一次定时器任务
+        this._timer = setInterval(
+            this._updateTimeString.bind(this),
+            5000
+        )
+    }
+    componentWillUnmount() {
+        // 移除之前先清除定时器
+        clearInterval(this._timer);
+    }
+
+    _updateTimeString() {
+        const comment = this.props.comment;
+        const duration = (+new Date() - comment.createdTime)/1000;
+        this.setState({
+            timeString: duration > 60 ? `${Math.round(duration/60)} 分钟前` : `${Math.round(Math.max(duration,1))} 秒前`
+        });
+    }
+
     render() {
         return (
             <div className='comment'>
@@ -8,6 +30,7 @@ export class CommentItem extends Component {
               <span>{this.props.comment.username} </span>：
             </div>
             <p>{this.props.comment.content}</p>
+            <span className='comment-createdtime'>{this.state.timeString}</span>
           </div>
         )
     }
